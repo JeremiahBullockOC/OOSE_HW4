7b4cef9d1a876ece0693160e386e0308be3f189e
Jeremiah Bullock <jeremiah.r.bullock-1@ou.edu>
2023-04-11 15:50:44 -0500

    Adding github actions

--- src/TestableUnitTest.java
+++ src/TestableUnitTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class TestableUnitTest {
